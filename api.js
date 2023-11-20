ce59de65917d4104aca70639afe4d259bc02c187
fauziramdaniii <fauziramdhan38@gmail.com>
2023-11-20 13:56:07 +0700

    CRUD API POSTGRES

---
+++ api.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {check, validationResult} = require("express-validator")
+const {isMobilePhone, isEmail} = require("validator")
+
+const client = require("./commons/connection");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log("server is running in port" + port);
+});
+
+client.connect(err => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("database connected");
+    }
+});
+
+const handleValidate = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()})
+    }
+        next()
+}
+
+app.get('/contacts', (req, res) => {
+    client.query('SELECT * FROM contact', (err, result) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        } else {
+            console.log(result.rows);
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/contacts',[
+    check("name").matches(/^[A-Za-z\s]+$/).withMessage("Name should contain only letters and spaces"),
+    check("email").isEmail().withMessage("Email is not valid"),
+    check("phone").custom(value => isMobilePhone(value, "id-ID")).withMessage("Phone Number is not valid"),
+], handleValidate, (req, res) => {
+    const { name, phone, email, address } = req.body;
+
+    const query = {
+        text: 'INSERT INTO contact (name, phone, email, address) VALUES ($1, $2, $3, $4)',
+        values: [name, phone, email, address]
+    };
+
+    client.query(query, (err, result) => {
+        if (!err) {
+            res.status(201).send("Insert Success");
+        } else {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+});
+
+app.put('/contacts/:id', [
+    check("name").matches(/^[A-Za-z\s]+$/).withMessage("Name should contain only letters and spaces"),
+    check("email").isEmail().withMessage("Email is not valid"),
+    check("phone").custom(value => isMobilePhone(value, "id-ID")).withMessage("Phone Number is not valid"),
+], handleValidate,(req, res) => {
+    const { name, phone, email, address } = req.body;
+    const contactId = req.params.id;
+
+    const query = {
+        text: 'UPDATE contact SET name = $1, phone = $2, email = $3, address = $4 WHERE id = $5',
+        values: [name, phone, email, address, contactId]
+    };
+
+    client.query(query, (err, result) => {
+        if (!err) {
+            res.status(200).send("Update Success");
+        } else {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+});
+
+app.delete('/contacts/:id', (req, res) => {
+    const contactId = req.params.id;
+    const query = {
+        text: 'DELETE FROM contact WHERE id = $1',
+        values: [contactId]
+    };
+
+    client.query(query, (err, result) => {
+        if (!err) {
+            res.status(200).send("Delete Success");
+        } else {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+});
+
